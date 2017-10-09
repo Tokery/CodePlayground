3cd26417366240b9b4b2f0409e7b2b54deb1c21a
Kevin Cottington <kevin.cottington@honeywell.com>
2017-10-09 00:02:25 -0400

    Further improvements to login experience

--- dundasbudget/src/components/App.jsx
+++ dundasbudget/src/components/App.jsx
@@ -27,14 +27,13 @@ export default class App extends Component {
  
     this.state = {
       hideCompleted: false,
-      currentUser: 'Kevin',
-      loggedIn: true,
+      currentUser: null,
+      loggedOut: true,
       tasks: []
     };
   }
 
   componentWillMount() {
-    console.log('App mounting');
     var that = this;
     axios.get('/transaction')
       .then(function(response) {
@@ -48,7 +47,30 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    
+    firebase.auth().onAuthStateChanged(user => {
+      console.log('Hit auth state changed');
+      if (user) {
+        console.log('Logged in');
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var phoneNumber = user.phoneNumber;
+        var providerData = user.providerData;
+        this.setState({
+          loggedOut: false,
+          currentUser: displayName
+        })
+      }
+      else {
+        console.log('Not logged in');
+        this.setState({
+          loggedOut: true,
+          currentUser: null
+        })
+      }
+    });
   }
 
   handleLogin() {
@@ -65,18 +87,19 @@ export default class App extends Component {
         <div className="col-md-4">
         </div>
         <div className="col-md-4 offset-md-4" style={{textAlign:'center', marginTop: '10em'}}>
-          <h1 style={{textAlign: 'center'}}>Expense Tracker</h1>
-
-          <div id="main-app">
-            <AuthComponent/>
-          </div>
-          <div className="container">
-            <Transactions 
-            tasks={this.state.tasks} 
-            incompleteCount={this.props.incompleteCount}
-            type="Expenses"
-            currentUser= {this.state.currentUser}/>        
-          </div>
+          <h1 style={{textAlign: 'center'}}>Eiditic: Remember Everything</h1>
+          { this.state.loggedOut ? 
+            <div id="main-app">
+              <AuthComponent/>
+            </div> :
+            <div className="container">
+              <Transactions 
+              tasks={this.state.tasks} 
+              incompleteCount={this.props.incompleteCount}
+              type="Expenses"
+              currentUser= {this.state.currentUser}/>        
+            </div>
+          }
         </div>
       </div>
     );
